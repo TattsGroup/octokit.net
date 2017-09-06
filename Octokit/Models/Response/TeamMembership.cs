d04a4ac96e31a60bef72c898fba3a1a84b900662
Ryan Gribble <ryan.gribble@gmail.com>
2017-09-06 13:27:46 +1000

    safekeeping

--- Octokit/Models/Response/TeamMembership.cs
+++ Octokit/Models/Response/TeamMembership.cs
@@ -1,9 +1,48 @@
-﻿namespace Octokit
+﻿using Octokit.Internal;
+
+namespace Octokit
 {
+    public class TeamMembershipResponse
+    {
+        public StringEnum<TeamRole> Role { get; set; }
+
+        public StringEnum<MembershipState> State { get; set; }
+    }
+
     public enum TeamMembership
     {
         NotFound = 0,
         Pending = 1,
         Active = 2
+    }
+
+    public enum MembershipState
+    {
+        [Parameter(Value = "pending")]
+        Pending,
+
+        [Parameter(Value = "active")]
+        Active
+    }
+
+    public enum TeamRole
+    {
+        [Parameter(Value = "member")]
+        Member,
+
+        [Parameter(Value = "maintainer")]
+        Maintainer
+    }
+
+    public enum TeamRoleFilter
+    {
+        [Parameter(Value = "member")]
+        Member,
+
+        [Parameter(Value = "maintainer")]
+        Maintainer,
+
+        [Parameter(Value = "all")]
+        All
     }
 }
